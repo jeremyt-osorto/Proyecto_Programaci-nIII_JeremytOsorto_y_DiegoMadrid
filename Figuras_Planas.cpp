dbabce263a59defa1bcea061eaba4c660fa13c04
jeremyt-osorto <jeremyt_osorto@unitec.edu>
2025-06-14 20:24:14 -0600

    Trata ahora

--- Figuras_Planas.cpp
+++ Figuras_Planas.cpp
@@ -11,12 +11,5 @@ int main(int argc, char* argv[]) {
     SetConsoleOutputCP(1252);
 	Procesador procesador;
 
-    char buffer[MAX_PATH];
-    GetCurrentDirectoryA(MAX_PATH, buffer);
-    std::cout << "Directorio actual: " << buffer << "\n";
-
-    std::string ruta_prueba = "Recursos/paralelogramo.txt";
-    std::ifstream test(ruta_prueba);
-    std::cout << "Archivo existe: " << (test.good() ? "Sí" : "No") << "\n";
     return procesador.ProcesarArgumentos(argc, argv);
 }
